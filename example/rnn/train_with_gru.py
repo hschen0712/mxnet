dc97b2c25fea05cd1a51ec0c25d319d8328e34d7
hschen0712 <hschen0712@gmail.com>
2016-11-24 17:30:50 +0800

    add mask to language model

--- example/rnn/train_with_gru.py
+++ example/rnn/train_with_gru.py
@@ -5,34 +5,40 @@
 """
 
 import os
-
 import numpy as np
 import mxnet as mx
+from mxnet.metric import EvalMetric
 
-from bucket_io import BucketSentenceIter, default_build_vocab
+from bucket_io_kmeans import BucketSentenceIter, default_build_vocab
 
 
 data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
 
-
+# this function only calculates the ppl of last batch, while our goal is to
+# calculate the average ppl of all batches
 def Perplexity(label, pred):
     # TODO(tofix): we make a transpose of label here, because when
     # using the RNN cell, we called swap axis to the data.
+    # label:  bs x n_step
+    # label.T.reshape((-1,)) means transpose and flatten
     label = label.T.reshape((-1,))
     loss = 0.
+    # pred.shape: (n_step * bs) x vocab_size
     for i in range(pred.shape[0]):
-        loss += -np.log(max(1e-10, pred[i][int(label[i])]))
+        # skip padding 0's
+        if int(label[i]) != 0:
+            loss += -np.log(max(1e-10, pred[i][int(label[i])]))
     return np.exp(loss / label.size)
 
 
 if __name__ == '__main__':
-    batch_size = 128
-    buckets = [10, 20, 30, 40, 50, 60]
+    batch_size = 32
     num_hidden = 200
     num_embed = 200
     num_lstm_layer = 2
+    num_buckets = 6
 
-    num_epoch = 2
+    num_epoch = 5
     learning_rate = 0.01
     momentum = 0.0
 
@@ -42,21 +48,26 @@ if __name__ == '__main__':
     init_h = [('GRU_init_h', (batch_size, num_lstm_layer, num_hidden))]
 
     data_train = BucketSentenceIter(os.path.join(data_dir, 'ptb.train.txt'),
-                                    vocab, buckets, batch_size, init_h)
+                                    vocab, num_buckets, batch_size, init_h)
     data_val = BucketSentenceIter(os.path.join(data_dir, 'ptb.valid.txt'),
-                                  vocab, buckets, batch_size, init_h)
+                                  vocab, num_buckets, batch_size, init_h)
 
     def sym_gen(seq_len):
+        # data, label, and sequence_length are provided in batch iterator
         data = mx.sym.Variable('data')
         label = mx.sym.Variable('softmax_label')
+        # shape: batch_size x seq_len x num_embed
         embed = mx.sym.Embedding(data=data, input_dim=len(vocab),
                                  output_dim=num_embed, name='embed')
-
         # TODO(tofix)
         # The inputs and labels from IO are all in batch-major.
         # Perform dimshuffle to convert them into time-major
         embed_tm = mx.sym.SwapAxis(embed, dim1=0, dim2=1)
         label_tm = mx.sym.SwapAxis(label, dim1=0, dim2=1)
+        sequence_length = mx.sym.Variable('sequence_length')
+        masked_embed = mx.sym.SequenceMask(data=embed_tm, sequence_length=sequence_length,
+                                           use_sequence_length=True, name="mask")
+
 
         # TODO(tofix)
         # Create transformed RNN initial states. Normally we do
@@ -86,7 +97,7 @@ if __name__ == '__main__':
 
         # RNN cell takes input of shape (time, batch, feature)
         # In GRU mode,we don;t need to provide argument state_cell
-        rnn = mx.sym.RNN(data=embed_tm, state_size=num_hidden,
+        rnn = mx.sym.RNN(data=masked_embed, state_size=num_hidden,
                          num_layers=num_lstm_layer, mode='gru',
                          name='gru',
                          # The following params can be omitted
@@ -111,27 +122,23 @@ if __name__ == '__main__':
                                      name='pred')
         sm = mx.sym.SoftmaxOutput(data=pred, label=label_cl, name='softmax')
 
-        data_names = ['data', 'GRU_init_h']
-        label_names = ['softmax_label']
+        return sm
 
-        return (sm, data_names, label_names)
+    model = mx.model.FeedForward(ctx=contexts,
+                                 symbol=sym_gen,
+                                 num_epoch=num_epoch,
+                                 learning_rate=learning_rate,
+                                 momentum=momentum,
+                                 wd=0.00001,
+                                 initializer=mx.init.Xavier(factor_type="in", magnitude=2.34))
 
-    if len(buckets) == 1:
-        mod = mx.mod.Module(*sym_gen(buckets[0]), context=contexts)
-    else:
-        mod = mx.mod.BucketingModule(sym_gen, default_bucket_key=data_train.default_bucket_key,
-                                     context=contexts)
+    # mod = mx.mod.BucketingModule(, default_bucket_key=data_train.default_bucket_key,
+    #                                  context=contexts)
 
     import logging
     head = '%(asctime)-15s %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=head)
 
-    mod.fit(data_train, eval_data=data_val, num_epoch=num_epoch,
-            eval_metric=mx.metric.np(Perplexity),
-            batch_end_callback=mx.callback.Speedometer(batch_size, 50),
-            initializer=mx.init.Xavier(factor_type="in", magnitude=2.34),
-            optimizer='adadelta')
-            # sgd is not stable
-            # optimizer='sgd',
-            # optimizer_params={'learning_rate': learning_rate,
-            #                   'momentum': momentum, 'wd': 0.00001})
+    model.fit(X=data_train, eval_data=data_val,
+              eval_metric=mx.metric.np(Perplexity),
+              batch_end_callback=mx.callback.Speedometer(batch_size, 50), )
